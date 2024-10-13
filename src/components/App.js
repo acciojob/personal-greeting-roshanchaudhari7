38930832f862eb07a0546165dd018355e26129c2
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2024-10-13 17:11:26 +0530

    Code Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text, setText] = useState("")
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+
+      <label htmlFor="username">Enter your name: </label>
+      <br />
+      <input type="text" id="username" onChange={(e) => {
+        setText(e.target.value);
+      }}/>
+
+      <p>{text && `Hello ${text}!`}</p>
     </div>
   )
 }
